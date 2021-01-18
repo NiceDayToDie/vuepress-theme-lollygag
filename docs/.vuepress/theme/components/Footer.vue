047ffda95454f8c6ec2beefaaa36bc8b51291c7d
Zane Shaw <295384295@qq.com>
2021-01-18 11:34:07 +0800

    [M] footer样式调整

--- docs/.vuepress/theme/components/Footer.vue
+++ docs/.vuepress/theme/components/Footer.vue
@@ -1,220 +1,225 @@
 <template>
-  <footer class="footer">
-    <div class="footer-left-wrap">
-      <ul v-if="contact" class="contact">
-        <li
-          v-for="item in contact"
-          :key="item.iconComponent"
-          class="contact-item"
-        >
-          <NavLink :link="item.link">
-            <component :is="item.iconComponent"></component>
-            {{ item.text }}
-          </NavLink>
-        </li>
-      </ul>
-    </div>
+    <footer class="footer">
+        <div class="footer-left-wrap">
+            <ul v-if="contact" class="contact">
+                <li
+                    v-for="item in contact"
+                    :key="item.iconComponent"
+                    class="contact-item"
+                >
+                    <NavLink :link="item.link">
+                        <component :is="item.iconComponent"></component>
+                        {{ item.text }}
+                    </NavLink>
+                </li>
+            </ul>
+        </div>
 
-    <div class="footer-right-wrap">
-      <ul v-if="copyright" class="copyright">
-        <li v-for="item in copyright" :key="item.text" class="copyright-item">
-          <NavLink v-if="item.link" :link="item.link">{{ item.text }}</NavLink>
-          <template v-else>{{ item.text }}</template>
-        </li>
-      </ul>
-    </div>
-  </footer>
+        <div class="footer-right-wrap">
+            <ul v-if="copyright" class="copyright">
+                <li v-for="item in copyright" :key="item.text" class="copyright-item">
+                    <NavLink v-if="item.link" :link="item.link">{{ item.text }}</NavLink>
+                    <template v-else>{{ item.text }}</template>
+                </li>
+            </ul>
+        </div>
+    </footer>
 </template>
 
 <script>
-import {
-  CodepenIcon,
-  CodesandboxIcon,
-  FacebookIcon,
-  GithubIcon,
-  GitlabIcon,
-  GlobeIcon,
-  InstagramIcon,
-  LinkedinIcon,
-  MailIcon,
-  MessageSquareIcon,
-  MusicIcon,
-  PhoneIcon,
-  TwitterIcon,
-  VideoIcon,
-  YoutubeIcon,
-} from 'vue-feather-icons'
+    import {
+        CodepenIcon,
+        CodesandboxIcon,
+        FacebookIcon,
+        GithubIcon,
+        GitlabIcon,
+        GlobeIcon,
+        InstagramIcon,
+        LinkedinIcon,
+        MailIcon,
+        MessageSquareIcon,
+        MusicIcon,
+        PhoneIcon,
+        TwitterIcon,
+        VideoIcon,
+        YoutubeIcon,
+    } from 'vue-feather-icons'
 
-export default {
-  components: {
-    CodepenIcon,
-    CodesandboxIcon,
-    FacebookIcon,
-    GithubIcon,
-    GitlabIcon,
-    GlobeIcon,
-    InstagramIcon,
-    LinkedinIcon,
-    MailIcon,
-    MessageSquareIcon,
-    MusicIcon,
-    PhoneIcon,
-    TwitterIcon,
-    VideoIcon,
-    YoutubeIcon,
-  },
+    export default {
+        components: {
+            CodepenIcon,
+            CodesandboxIcon,
+            FacebookIcon,
+            GithubIcon,
+            GitlabIcon,
+            GlobeIcon,
+            InstagramIcon,
+            LinkedinIcon,
+            MailIcon,
+            MessageSquareIcon,
+            MusicIcon,
+            PhoneIcon,
+            TwitterIcon,
+            VideoIcon,
+            YoutubeIcon,
+        },
 
-  computed: {
-    contact() {
-      return (
-        (this.$themeConfig.footer && this.$themeConfig.footer.contact) ||
-        []
-      )
-        .map(({ type, link }) => {
-          return {
-            iconComponent: this.getIconComponentName(type),
-            link,
-          }
-        })
-        .filter(({ iconComponent }) => iconComponent)
-    },
+        computed: {
+            contact() {
+                return (
+                    (this.$themeConfig.footer && this.$themeConfig.footer.contact) ||
+                    []
+                )
+                    .map(({type, link}) => {
+                        return {
+                            iconComponent: this.getIconComponentName(type),
+                            link,
+                        }
+                    })
+                    .filter(({iconComponent}) => iconComponent)
+            },
 
-    copyright() {
-      return (
-        (this.$themeConfig.footer && this.$themeConfig.footer.copyright) || []
-      )
-    },
-  },
+            copyright() {
+                return (
+                    (this.$themeConfig.footer && this.$themeConfig.footer.copyright) || []
+                )
+            },
+        },
 
-  methods: {
-    getIconComponentName(contactType) {
-      switch (contactType) {
-        case 'codepen':
-          return 'CodepenIcon'
-        case 'codesandbox':
-          return 'CodesandboxIcon'
-        case 'facebook':
-          return 'FacebookIcon'
-        case 'github':
-          return 'GithubIcon'
-        case 'gitlab':
-          return 'GitlabIcon'
-        case 'instagram':
-          return 'InstagramIcon'
-        case 'linkedin':
-          return 'LinkedinIcon'
-        case 'mail':
-          return 'MailIcon'
-        case 'messenger':
-          return 'MessageSquareIcon'
-        case 'music':
-          return 'MusicIcon'
-        case 'phone':
-          return 'PhoneIcon'
-        case 'twitter':
-          return 'TwitterIcon'
-        case 'video':
-          return 'VideoIcon'
-        case 'web':
-          return 'GlobeIcon'
-        case 'youtube':
-          return 'YoutubeIcon'
-        default:
-          return ''
-      }
-    },
-  },
-}
+        methods: {
+            getIconComponentName(contactType) {
+                switch (contactType) {
+                    case 'codepen':
+                        return 'CodepenIcon'
+                    case 'codesandbox':
+                        return 'CodesandboxIcon'
+                    case 'facebook':
+                        return 'FacebookIcon'
+                    case 'github':
+                        return 'GithubIcon'
+                    case 'gitlab':
+                        return 'GitlabIcon'
+                    case 'instagram':
+                        return 'InstagramIcon'
+                    case 'linkedin':
+                        return 'LinkedinIcon'
+                    case 'mail':
+                        return 'MailIcon'
+                    case 'messenger':
+                        return 'MessageSquareIcon'
+                    case 'music':
+                        return 'MusicIcon'
+                    case 'phone':
+                        return 'PhoneIcon'
+                    case 'twitter':
+                        return 'TwitterIcon'
+                    case 'video':
+                        return 'VideoIcon'
+                    case 'web':
+                        return 'GlobeIcon'
+                    case 'youtube':
+                        return 'YoutubeIcon'
+                    default:
+                        return ''
+                }
+            },
+        },
+    }
 </script>
 
 <style lang="stylus" scoped>
-ol, ul
-  list-style none
-  margin 0
-  padding 0
-  line-height 1
+    @require "~@/styles/mixins.styl"
 
-.footer
-  width 100%
-  position fixed
-  bottom 0
-  min-height $footerHeight
-  box-sizing border-box
-  background-color $footerBgColor
-  display flex
-  padding 15px 32px
+    ol, ul
+        list-style none
+        margin 0
+        padding 0
+        line-height 1
 
-  .footer-left-wrap
-    line-height 30px
-    flex 1
-    display flex
+    .footer
+        width 100%
+        position fixed
+        bottom 0
+        min-height $footerHeight
+        box-sizing border-box
+        background-color $footerBgColor
+        u-flex row, center, center
+        padding 0 32px
 
-    .contact
-      display flex
-      flex-wrap wrap
-      justify-content center
+        .footer-left-wrap
+            height 24px
+            line-height 24px
+            flex 1
+            display flex
 
-      .contact-item
-        margin-right 10px
+            .contact
+                display flex
+                flex-wrap wrap
+                justify-content center
 
-        a
-          font-size 12px
-          color $footerColor
-          text-decoration none
-          transition color 0.3s
+                .contact-item
+                    margin-right 10px
 
-          &:hover
-            color #FFF
+                    a
+                        font-size 12px
+                        color $footerColor
+                        text-decoration none
+                        transition color 0.3s
 
-  .footer-right-wrap
-    flex 1
-    display flex
-    align-items center
-    justify-content center
+                        &:hover
+                            color #FFF
 
-    @media (min-width: $MQNarrow)
-      justify-content flex-end
+        .footer-right-wrap
+            flex 1
+            display flex
+            align-items center
+            justify-content center
 
-    .copyright
-      display flex
-      justify-content flex-end
+            @media (min-width: $MQNarrow)
+                justify-content flex-end
 
-      .copyright-item
-        flex 0 0 auto
-        padding 0 10px
-        position relative
-        line-height 12px
-        border-right 1px solid $footerColor
-        font-size 12px
-        color $footerColor
+            .copyright
+                display flex
+                justify-content flex-end
 
-        &:last-child
-          border-right none
+                .copyright-item
+                    flex 0 0 auto
+                    padding 0 10px
+                    position relative
+                    line-height 12px
+                    border-right 1px solid $footerColor
+                    font-size 12px
+                    color $footerColor
 
-        a
-          color currentColor
-          text-decoration none
-          transition color 0.3s
+                    &:last-child
+                        border-right none
 
-          &:hover
-            color rgba(255, 255, 255, 0.9)
+                    a
+                        color currentColor
+                        text-decoration none
+                        transition color 0.3s
 
-@media (max-width: $MQMobile)
-  .footer
-    position static
-    min-height 100px
-    flex-direction column
+                        &:hover
+                            color rgba(255, 255, 255, 0.9)
 
-    .footer-left-wrap
-      align-items center
-      justify-content center
+    @media (max-width: $MQMobile)
+        .footer
+            position static
+            min-height 100px
+            flex-direction column
 
-    .footer-right-wrap
-      .copyright
-        display block
+            .footer-left-wrap
+                flex none
+                margin-top 20px
+                align-items center
+                justify-content center
 
-        .copyright-item
-          line-height 1
-          border-right none
-          text-align center
+            .footer-right-wrap
+                .copyright
+                    display block
+
+                    .copyright-item
+                        line-height 1
+                        border-right none
+                        text-align center
 </style>
