1293ffc4bc27a274317492e23dcdc3580e836673
Zane Shaw <295384295@qq.com>
2021-03-01 11:18:40 +0800

    [A] 新增Spoiler组件

---
+++ docs/.vuepress/theme/global-components/Spoiler.vue
@@ -0,0 +1,19 @@
+<template>
+    <span class="spoiler"><slot></slot></span>
+</template>
+
+<script>
+    export default {
+        name: "Spoiler"
+    }
+</script>
+
+<style scoped lang="stylus">
+    .spoiler
+        color #000;
+        cursor pointer;
+        background #000;
+
+        &:hover
+            color #FFFFFF;
+</style>
